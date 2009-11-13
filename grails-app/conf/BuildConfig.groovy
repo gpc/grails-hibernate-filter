d47cbf6316bb273d6554d76920def2f5ea1aa8ff
Scott Burch <scott@bulldoginfo.com>
2009-11-13 03:16:54 +0000

    trying to release

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,6 @@
+grails.plugin.repos.discovery.grailsplugins="https://scottburch:HFQsIduda@svn.codehaus.org/grails-plugins"
+user.svn.username.discovery.grailsplugins="scottburch"
+user.svn.password.discovery.grailsplugins="HFQsIduda"
+grails.plugin.repos.distribution.grailsplugins="https://scottburch:HFQsIduda@svn.codehaus.org/grails-plugins"
+user.svn.username.distribution.grailsplugins="scottburch"
+user.svn.password.distribution.grailsplugins="HFQsIduda"
