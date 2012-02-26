588a3441a32dfa6bcbb6782decd7fed86724c4c4
Burt Beckwith <burt@burtbeckwith.com>
2012-02-26 02:37:38 +0000

    upgrade to 2.0 for dev

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,13 @@
+grails.project.work.dir = 'target'
+
+grails.project.dependency.resolution = {
+
+	inherits 'global'
+	log 'warn'
+
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
+	}
+}
---
+++ grails-app/conf/Config.groovy
@@ -0,0 +1,7 @@
+log4j = {
+	error 'org.codehaus.groovy.grails',
+			'org.springframework',
+			'org.hibernate',
+			'net.sf.ehcache.hibernate'
+	debug 'org.grails.plugin.hibernate.filter'
+}
