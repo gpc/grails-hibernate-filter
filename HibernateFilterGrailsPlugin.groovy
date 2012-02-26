626fd920ddeffe2512b217111feed4afa166d514
Burt Beckwith <burt@burtbeckwith.com>
2012-02-26 02:50:36 +0000

    cleanup

--- HibernateFilterGrailsPlugin.groovy
+++ HibernateFilterGrailsPlugin.groovy
@@ -1,111 +1,77 @@
-import org.codehaus.groovy.grails.commons.ApplicationHolder
+import org.codehaus.groovy.grails.commons.GrailsDomainClass
 import org.grails.plugin.hibernate.filter.DefaultHibernateFiltersHolder
 import grails.util.GrailsUtil
 
 class HibernateFilterGrailsPlugin {
-    // the plugin version
-    def version = "0.2"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.1 > *"
-    // the other plugins this plugin depends on
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
+	def version = '0.2'
+	def grailsVersion = '1.1 > *'
 
-    // removed after issues with dependencies
-//    def dependsOn = [controllers: '1.1 > *' , hibernate: '0.1 > *']
+	def loadAfter = ['controllers', 'hibernate']
+	def observe = ['controllers']
 
-    def loadAfter = ['controllers', 'hibernate']
-    def observe = ['controllers']
+	def author = 'Scott Burch'
+	def authorEmail = 'scott@bulldoginfo.com'
+	def title = 'Hibernate Filter plugin'
+	def description = 'Integrates Hibernate filtering into Grails'
+	def documentation = 'http://grails.org/plugin/hibernate-filter'
 
-    // TODO Fill in these fields
-    def author = "Scott Burch"
-    def authorEmail = "scott@bulldoginfo.com"
-    def title = "Integrates Hibernate filtering into Grails"
-    def description = '''\\
-Integrates Hibernate filtering into Grails
-'''
+	def doWithDynamicMethods = { ctx ->
+		for (GrailsDomainClass dc in application.domainClasses) {
+			addMethods dc.clazz, ctx
+		}
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/HibernateFilter+Plugin"
+		for (Class artefactClass in application.allArtefacts) {
+			addDomainProxies artefactClass
+		}
+	}
 
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
+	def onChange = { event ->
+		if (event?.source) {
+			addDomainProxies(event.source)
+		}
+	}
 
-    def doWithApplicationContext = {applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
+	private void addDomainProxies(clazz) {
+		for (HibernateFilterDomainProxy proxy in DefaultHibernateFiltersHolder.domainAliasProxies) {
+			clazz.metaClass."$proxy.aliasName" = proxy
+		}
+	}
 
-    def doWithWebDescriptor = {xml ->
-        // TODO Implement additions to web.xml (optional)
-    }
+	private void addMethods(clazz, ctx) {
+		clazz.metaClass.static.withHibernateFilter = { String name, Closure closure ->
+			def session = ctx.sessionFactory.currentSession
+			def isFilterDisabled = session.getEnabledFilter(name) == null
+			try {
+				session.enableFilter name
+				closure.doCall()
+			}
+			finally {
+				if (isFilterDisabled) {
+					session.disableFilter name
+				}
+			}
+		}
 
-    def doWithDynamicMethods = {ctx ->
-        application.DomainClasses.each {artefact ->
-            addMethods(artefact, ctx)
-        }
-        application.allArtefacts.each {artefact ->
-            addDomainProxies(artefact)
-        }
-    }
+		clazz.metaClass.static.withoutHibernateFilter = { String name, Closure closure ->
+			def session = ctx.sessionFactory.currentSession
+			def isFilterEnabled = session.getEnabledFilter(name) != null
+			try {
+				session.disableFilter(name)
+				closure.doCall()
+			}
+			finally {
+				if (isFilterEnabled) {
+					session.enableFilter name
+				}
+			}
+		}
 
-    def onChange = {event ->
-        if(event?.source) {
-            addDomainProxies(event.source)
-        }
-    }
+		clazz.metaClass.static.enableHibernateFilter = { String name ->
+			ctx.sessionFactory.currentSession.enableFilter name
+		}
 
-    def onConfigChange = {event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    private addDomainProxies(clazz) {
-
-        DefaultHibernateFiltersHolder.domainAliasProxies.each {proxy ->
-            clazz.metaClass."${proxy.aliasName}" = proxy
-        }
-    }
-
-    def addMethods(clazz, ctx) {
-        clazz.metaClass.'static'.withHibernateFilter = {name, closure ->
-            def session = ctx.sessionFactory.currentSession
-            def isFilterDisabled = session.getEnabledFilter(name) == null
-            try {
-                session.enableFilter(name)
-                closure.doCall()
-            } catch (e) {
-                throw e
-            } finally {
-                if(isFilterDisabled) {
-                    session.disableFilter(name)
-                }
-            }
-        }
-
-        clazz.metaClass.'static'.withoutHibernateFilter = {name, closure ->
-            def session = ctx.sessionFactory.currentSession
-            def isFilterEnabled = session.getEnabledFilter(name) != null
-            try {
-                session.disableFilter(name)
-                closure.doCall()
-            } catch (e) {
-                throw e
-            } finally {
-                if (isFilterEnabled) {
-                    session.enableFilter(name)
-                }
-            }
-        }
-
-        clazz.metaClass.'static'.enableHibernateFilter = {name ->
-            ctx.sessionFactory.currentSession.enableFilter(name)
-        }
-
-        clazz.metaClass.'static'.disableHibernateFilter = {name ->
-            ctx.sessionFactory.currentSession.disableFilter(name)
-        }
-    }
+		clazz.metaClass.static.disableHibernateFilter = { String name ->
+			ctx.sessionFactory.currentSession.disableFilter name
+		}
+	}
 }
-
