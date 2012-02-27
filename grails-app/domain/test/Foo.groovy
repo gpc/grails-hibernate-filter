5530040571267d511923a92701232012475f36bf
Burt Beckwith <burt@burtbeckwith.com>
2012-02-27 00:08:47 +0000

    unit tests for HibernateFilterBuilder

--- grails-app/domain/test/Foo.groovy
+++ grails-app/domain/test/Foo.groovy
@@ -11,6 +11,10 @@ class Foo {
 		fooEnabledFilter(condition: 'enabled=1', default: true, aliasDomain: 'EnabledFoo')
 		barEnabledFilter(collection: 'bars', condition: 'enabled=1', default: true)
 		fooNameFilter(condition: ':name = name', types: 'string')
+		closureDefaultFilter(condition: 'enabled=1', default: { -> false })
+		inListFilter(condition: '(organisation_id = :oragnisationId or organisation_id in (:organisationIds))',
+		             default: false,
+						 paramTypes: 'long, long')
 	}
 
 	String toString() {
