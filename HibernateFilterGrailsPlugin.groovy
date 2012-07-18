e9a9b8f674d9285aa9ba97bc9a51a78cb7978991
Burt Beckwith <beckwithb@vmware.com>
2012-07-18 14:49:45 -0400

    0.3.2 release

--- HibernateFilterGrailsPlugin.groovy
+++ HibernateFilterGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import org.grails.plugin.hibernate.filter.DefaultHibernateFiltersHolder
 import org.grails.plugin.hibernate.filter.HibernateFilterUtils
 
 class HibernateFilterGrailsPlugin {
-	def version = '0.3.1'
+	def version = '0.3.2'
 	def grailsVersion = '1.3 > *'
 	def loadAfter = ['controllers', 'hibernate']
 	def observe = ['*']
