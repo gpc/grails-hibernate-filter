642724426e7cb6c0c045e7be72dc10b9e8f96cb8
Jonas Stenberg <jonas.stenberg@21grams.com>
2014-02-21 14:47:15 +0100

    Update HibernateFilterFilters.groovy
    At least makes it possible to have webflow plugin and hibernate filter in the same project even if it still does not work to use them in the same controller. 
    
    Help me out here, this code is far from perfect but it is the best I can do to get things running at all.
    
    Basically the same changes as proposed by Aaron Long earlier, but that was never sent as a pull request so I make a new try to get some attention.
    
    https://github.com/longwa/grails-hibernate-filter/commit/25096b1265050d61b4069777370489b36b599039

--- grails-app/conf/HibernateFilterFilters.groovy
+++ grails-app/conf/HibernateFilterFilters.groovy
@@ -1,4 +1,7 @@
 import org.grails.plugin.hibernate.filter.DefaultHibernateFiltersHolder
+import org.hibernate.HibernateException
+import org.codehaus.groovy.grails.web.context.ServletContextHolder
+
 
 class HibernateFilterFilters {
 
@@ -7,11 +10,28 @@ class HibernateFilterFilters {
 	def filters = {
 		enableHibernateFilters(controller:'*', action:'*') {
 			before = {
-				def session = sessionFactory.currentSession
-				for (String name in DefaultHibernateFiltersHolder.defaultFilters) {
-					session.enableFilter name
-				}
+		                def session = getHibernateSession()
+		                if(session) {
+					for (String name in DefaultHibernateFiltersHolder.defaultFilters) {
+						session.enableFilter name
+					}
+		                }
 			}
 		}
 	}
+	
+    private getHibernateSession() {
+        def session
+        try {
+            // Get via the normal session factory
+            session = sessionFactory.currentSession
+        }
+        catch(HibernateException ignored) {
+            // If this is webflow, try to return the session from flow scope
+            session = ServletContextHolder.servletContext?.flowScope?.get("session")
+        }
+
+        return session
+    }
+	
 }
