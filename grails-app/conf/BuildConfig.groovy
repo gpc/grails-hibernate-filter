2a5cc512732076053bf0be255a85f52bfb1cba33
Ivan Chang <ichang@appcela.com>
2014-09-24 01:18:44 +0800

    updated to work with Grails 2.4.3 and Hibernate 4.x

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,6 @@
 grails.project.work.dir = 'target'
 grails.project.source.level = 1.6
+grails.project.dependency.resolver = "maven" // switched to Maven instead of Ivy
 
 grails.project.dependency.resolution = {
 
@@ -10,10 +11,12 @@ grails.project.dependency.resolution = {
 		grailsCentral()
 	}
 
-	dependencies {}
+	dependencies {
+		//compile 'org.hibernate:hibernate-core:4.3.6.Final'
+	}
 
 	plugins {
-		build(':release:2.0.3', ':rest-client-builder:1.0.2') {
+		build (':release:3.0.1', ':hibernate4:4.3.5.5', ':rest-client-builder:2.0.3') { // updated plugin versions
 			export = false
 		}
 	}
