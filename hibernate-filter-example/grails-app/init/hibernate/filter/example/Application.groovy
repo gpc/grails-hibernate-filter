ff134c9b4b9b1dc32c71f20b31d5a5d8c5a47312
Piotr Chowaniec <piotr.chowaniec@fingo.pl>
2015-11-20 21:41:40 +0100

    grails-hibernate-filter-3 Move integration tests into a separate project
    added simple example application

---
+++ hibernate-filter-example/grails-app/init/hibernate/filter/example/Application.groovy
@@ -0,0 +1,10 @@
+package hibernate.filter.example
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
