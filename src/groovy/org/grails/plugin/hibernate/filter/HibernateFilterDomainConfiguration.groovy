aac7360a77080415eb7e82dde75706cf9450e759
Dónal Murtagh <donal.murtagh@cognovi.ie>
2016-02-18 16:29:09 +0000

    reorganised this class so that the Hibernate config. customisation (adding filters) can easily be extended/replaced in a subclass

--- src/groovy/org/grails/plugin/hibernate/filter/HibernateFilterDomainConfiguration.groovy
+++ src/groovy/org/grails/plugin/hibernate/filter/HibernateFilterDomainConfiguration.groovy
@@ -1,5 +1,6 @@
 package org.grails.plugin.hibernate.filter
 
+import org.codehaus.groovy.grails.commons.GrailsDomainClass
 import org.codehaus.groovy.grails.orm.hibernate.cfg.GrailsAnnotationConfiguration
 import org.codehaus.groovy.grails.commons.GrailsApplication
 import org.hibernate.MappingException
@@ -24,13 +25,22 @@ class HibernateFilterDomainConfiguration extends GrailsAnnotationConfiguration {
 			return
 		}
 
-		for (domainClass in grailsApplication.domainClasses) {
-			def filters = domainClass.getPropertyValue('hibernateFilters')
-			if (filters instanceof Closure) {
-				new HibernateFilterBuilder(this, domainClass)
-			}
-		}
-
+        customiseConfiguration(grailsApplication)
 		configLocked = true
 	}
+
+    /**
+     * Do customisations of the Hibernate configuration which should only be performed once. The default implementation
+     * adds any filters defined in the domain classes, but a subclass could replace or supplement this with another
+     * once-off customisation e.g. <a href="http://burtbeckwith.com/blog/?p=465">changing the foreign key constraint names</a>
+     */
+    protected void customiseConfiguration(GrailsApplication grailsApplication) {
+
+        grailsApplication.domainClasses.each { GrailsDomainClass domainClass ->
+            def filters = domainClass.getPropertyValue('hibernateFilters')
+            if (filters instanceof Closure) {
+                new HibernateFilterBuilder(this, domainClass)
+            }
+        }
+    }
 }
