3f60fd4d5951317f899dd07a2cda5ccc48ffb1c2
Dónal Murtagh <donal.murtagh@cognovi.ie>
2016-03-03 12:49:34 +0000

    improved tests and changed @Issue references to refer to GitHub issue IDs

--- test/integration/org/grails/plugin/hibernate/filter/StatusFilterSpec.groovy
+++ test/integration/org/grails/plugin/hibernate/filter/StatusFilterSpec.groovy
@@ -1,20 +1,21 @@
 package org.grails.plugin.hibernate.filter
 
 import grails.test.spock.IntegrationSpec
+import org.hibernate.SessionFactory
 import spock.lang.Issue
 import test.domain.status.Condition
 import test.domain.status.Problem
 
 class StatusFilterSpec extends IntegrationSpec {
 
-    private Problem problem
-    private Condition condition
+    SessionFactory sessionFactory
 
     def setup() {
-        this.problem = new Problem(name: 'problem').save(failOnError: true)
-        this.condition = new Condition(name: 'condition').addToProblems(problem).save(failOnError: true)
+        [0, 1].each { status ->
+            new Problem(name: 'problem', status: status).save(failOnError: true)
+            new Condition(name: 'condition', status: status).save(failOnError: true)
+        }
 
-        // enable the Hibernate filters
         Problem.enableHibernateFilter('statusFilter')
         Condition.enableHibernateFilter('statusFilter')
     }
@@ -29,33 +30,44 @@ class StatusFilterSpec extends IntegrationSpec {
         Problem.count == 1
     }
 
-    @Issue('GPHIBERNATEFILTER-19')
+    @Issue('https://github.com/burtbeckwith/grails-hibernate-filter/issues/8')
     void 'query that joins problem and condition'() {
+
+        Condition.findByStatus(1).addToProblems(Problem.findByStatus(1))
 
         when:
         List<Problem> problems = Problem.executeQuery("""
                 SELECT  p
-                FROM    ${Problem.simpleName} p
+                FROM    Problem p
                 JOIN    p.conditions""")
 
         then:
         problems.size() == 1
     }
 
-    @Issue('GPHIBERNATEFILTER-20')
-    void 'collection filters are incorrectly applied to join table'() {
+    // this test is currently failing due to the error described in the issue below
+    @Issue('https://github.com/burtbeckwith/grails-hibernate-filter/issues/9')
+    void 'test collection hibernate filters'() {
 
-        // enable the collection filters in each class
+        setup: "Add a valid and invalid condition to a valid problem. When we retrieve the problem the invalid condition be inaccessible"
+        def validProblem = Problem.findByStatus(1)
+        new Condition(name: 'invalidCondition', status: 0).addToProblems(validProblem).save(failOnError: true)
+        new Condition(name: 'validCondition', status: 1).addToProblems(validProblem).save(failOnError: true)
+
+        sessionFactory.currentSession.clear()
+
+        // enable the collection filters
         Problem.enableHibernateFilter('collectionFilter')
         Condition.enableHibernateFilter('collectionFilter')
 
         when:
         List<Problem> problems = Problem.executeQuery("""
                 SELECT  p
-                FROM    ${Problem.simpleName} p
+                FROM    Problem p
                 JOIN    p.conditions""")
 
         then:
         problems.size() == 1
+        problems.conditions.size() == 1
     }
 }
